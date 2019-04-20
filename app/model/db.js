3832597aa49b5ff4a90adaeb24f48d462fe9fac0
dharmatv <rupeshdharma62@gmail.com>
2019-04-20 12:49:06 +0530

    change db crenditals related to heroku mysql

--- app/model/db.js
+++ app/model/db.js
@@ -3,10 +3,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host    :  'localhost',
-    user    :  'admin',
-    password:  'Djana@2349',
-    database:   'onmiway'
+    host    :  'ec2-54-247-70-127.eu-west-1.compute.amazonaws.com',
+    user    :  'axthzsbxorpjoq',
+    password:  'bbe0417783325ee27cc17c788166a0fa5edd128c7f792b21c7a90f1d088c61e8',
+    database:   'd2f237trkrilnt'
 });
 
 connection.connect(function(err){
