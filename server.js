55b464de44257ad6108b0a72b8f46f007cdb47d8
dharmatv <rupeshdharma62@gmail.com>
2019-04-20 12:34:11 +0530

    add remaining files

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+app = express();
+bodyParser = require('body-parser');
+port = process.env.PORT || 3000;
+
+const mysql = require('mysql');
+const mc = mysql.createConnection({
+    host    :  'localhost',
+    user    :  'admin',
+    password:  'Djana@2349',
+    database:   'onmiway'
+});
+ 
+// connect to database
+mc.connect();
+
+app.listen(port);
+
+console.log('API server started on: ' + port);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./app/routes/appRoutes'); //importing route
+routes(app); 
+
+console.log("restful API service is"+ port);
