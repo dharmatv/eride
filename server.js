2213002b4828aa4a979e6cbe07758999f7789776
Dharma <rupeshdharma62@gmail.com>
2019-04-23 07:34:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ app = express();
 bodyParser = require('body-parser');
 port = process.env.PORT || 3000;
 
-// const mysql = require('mysql');
-//const mc = mysql.createConnection({
-   // host    :  'localhost',
-   // user    :  'admin',
-    //password:  'Djana@2349',
-  //  database:   'onmiway'
-//});
+ const mysql = require('mysql');
+const mc = mysql.createConnection({
+     host    :  'ec2-54-247-70-127.eu-west-1.compute.amazonaws.com',
+    user    :  'axthzsbxorpjoq',
+    password:  'bbe0417783325ee27cc17c788166a0fa5edd128c7f792b21c7a90f1d088c61e8',
+    database:   'd2f237trkrilnt'
+});
  
-// connect to database
-//mc.connect();
+//connect to database
+mc.connect();
 
 app.listen(port);
 
